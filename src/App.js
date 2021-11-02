cc7181569618da7d5709e99d990c9124a36dc682
Jafar Li-hammed <lihammedjafar@gmail.com>
2021-11-02 13:20:50 -0700

    birthday reminder app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Data from './Data';
+import List from "./List";
 
 function App() {
+  const [people, setpeople] = useState(Data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main'>
+      <section className='container'>
+        <h3 className='head'>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setpeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,45 @@
+const Data = [
+  {
+    id: 1,
+    name: "Bertie Yates",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/dvo8xhx6r/image/upload/v1635850250/dwayne-the-rock-_nywovp.jpg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/dvo8xhx6r/image/upload/v1635850244/ANGELAPERSON-1447-300x300_gnifi2.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/dvo8xhx6r/image/upload/v1635850244/image_itnk1x.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/dvo8xhx6r/image/upload/v1635850246/send-money-in-person-from-jamaica-resp_p6pk5p.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/dvo8xhx6r/image/upload/v1635850258/Person-Curtis_4x5-e1564616444404-300x300_twe6th.jpg",
+  },
+  {
+    id: 5,
+    name: "Adelani Bade",
+    age: 36,
+    image: "https://res.cloudinary.com/dvo8xhx6r/image/upload/v1635850253/personnel-image-4483_xec4ls.jpg"
+  },
+];
+
+export default Data;
